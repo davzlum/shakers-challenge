c599dec943ef8ee889b53d4199fb45e1cf2eae53
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-17 19:50:05 +0200

    Connect with mongo database collection

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const cors = require('cors');
+const morgan = require('morgan');
 
 require('dotenv').config();
 
 const port = process.env.PORT || 2021;
+const originHost = process.env.ORIGIN_HOST || 'http://localhost:3000';
 
 const app = express();
 app.disable('x-powered-by');
+const corsOptions = {
+  origin: originHost,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(morgan('dev'));
+require('./ddbb/mongoose.config');
+
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port,
   () => debug(`Server is running in ${chalk.magentaBright(`localhost:${port}`)}`));

7ac61d0194f9bd8360b9ccf65c0df4a995822a8a
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-18 14:23:01 +0200

    Create ranking model

---
+++ server/src/model/rankingModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const RankingSchema = mongoose.Schema({
+  playerX: {
+    won: Number,
+    lost: Number,
+    tied: Number,
+  },
+  playerO: {
+    won: Number,
+    lost: Number,
+    tied: Number,
+  },
+});
+
+module.exports = mongoose.model('Ranking', RankingSchema);

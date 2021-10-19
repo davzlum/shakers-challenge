1cfb53b50432690578630e9f0cafeb1522bc2c42
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-19 14:59:12 +0200

    Create ranking table on ranking component

--- client/src/components/Ranking/index.jsx
+++ client/src/components/Ranking/index.jsx
@@ -1,14 +1,84 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './style.scss';
 
 const Ranking = () => {
-  const ranking = useSelector((store) => store.rankingReducer);
-  console.log(ranking);
+  const ranking = useSelector((store) => store.ranking);
+  const gameCounter = ranking.playerX
+    ? ranking.playerX.won + ranking.playerX.lost + ranking.playerX.tied
+    : 0;
   return (
-    <>
-      <Link to="/">Go back</Link>
-    </>
+    <div className="ranking-container">
+      {ranking.playerX
+        ? (
+          <table className="ranking-table">
+            <tr>
+              <td />
+              <td><div className="icon-x" /></td>
+              <td><div className="icon-o" /></td>
+            </tr>
+            <tr>
+              <th className="row-category">Won</th>
+              <td className="player-x">
+                {ranking.playerX.won}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+              </td>
+              <td className="player-o">
+                {ranking.playerO.won}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+
+              </td>
+            </tr>
+            <tr>
+              <th className="row-category">Lost</th>
+              <td className="player-x">
+                {ranking.playerX.lost}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+
+              </td>
+              <td className="player-o">
+                {ranking.playerO.lost}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+
+              </td>
+            </tr>
+            <tr>
+              <th className="row-category">Tied</th>
+              <td className="player-x">
+                {ranking.playerX.tied}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+
+              </td>
+              <td className="player-o">
+                {ranking.playerO.tied}
+                {' '}
+                /
+                {' '}
+                {gameCounter}
+
+              </td>
+            </tr>
+          </table>
+        )
+        : <div>There are no ranking to load, please go back to create a new game</div>}
+      <Link to="/"><div className="button">Go back</div></Link>
+    </div>
   );
 };
 

ac26f3a5c1ecce72db79aa49eda097d6107298ca
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-20 18:07:43 +0200

    Create logic for the IA checking winning positions

--- server/src/helpers/game.helper.js
+++ server/src/helpers/game.helper.js
@@ -27,4 +27,75 @@ const checkForWinner = (newSquares) => {
   return result;
 };
 
-module.exports = { checkForWinner };
+// const simulate = (actualSquares, playerToCheck) => {
+//   const fakeSquares = [...actualSquares];
+//   for (let i = 0; i < actualSquares.length; i += 1) {
+//     if (!actualSquares[i]) {
+//       fakeSquares[i] = playerToCheck;
+//       const result = checkForWinner(fakeSquares);
+//       if (result.isWinner) {
+//         return { isWinner: result.isWinner, position: i };
+//       }
+//       fakeSquares[i] = null;
+//     }
+//   }
+// };
+
+const autoPlay = (actualSquares) => {
+  const newSquares = [...actualSquares];
+
+  let isCpuWinner = false;
+  let isPlayerWinner = false;
+
+  for (let i = 0; i < actualSquares.length; i += 1) {
+    if (!actualSquares[i]) {
+      newSquares[i] = 'O';
+      const result = checkForWinner(newSquares);
+      if (result.isWinner) {
+        isCpuWinner = true;
+        break;
+      } else {
+        newSquares[i] = null;
+      }
+    }
+  }
+  // const cpuSimulation = simulate(actualSquares, 'O');
+  // const playerSimulation = simulate(actualSquares, 'X');
+
+  // if (cpuSimulation.isWinner) {
+  //   newSquares[cpuSimulation.position] = 'O';
+  // }
+
+  if (!isCpuWinner) {
+    // newSquares[playerSimulation.position] = 'O';
+    for (let i = 0; i < actualSquares.length; i += 1) {
+      if (!actualSquares[i]) {
+        newSquares[i] = 'X';
+        const result = checkForWinner(newSquares);
+        if (result.isWinner) {
+          isPlayerWinner = true;
+          newSquares[i] = 'O';
+          break;
+        } else {
+          newSquares[i] = null;
+        }
+      }
+    }
+  }
+  if (!isCpuWinner && !isPlayerWinner) {
+    // newSquares[cpuSimulation.position] = 'O';
+    for (let i = 0; i < actualSquares.length; i += 1) {
+      if (!actualSquares[4]) {
+        newSquares[4] = 'O';
+        break;
+      } else if (actualSquares[4] && !actualSquares[i]) {
+        newSquares[i] = 'O';
+        break;
+      }
+    }
+  }
+
+  return newSquares;
+};
+
+module.exports = { checkForWinner, autoPlay };

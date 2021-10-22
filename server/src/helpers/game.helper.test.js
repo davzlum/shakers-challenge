74fc1132c37ce5fbd3c9f0828a99bdacd11e462a
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-22 02:58:12 +0200

    Test game helper

--- server/src/helpers/game.helper.test.js
+++ server/src/helpers/game.helper.test.js
@@ -1,49 +1,42 @@
 const {
-  autoPlay,
+  autoPlay
 } = require('./game.helper');
 
 describe('Given an autoPlay function', () => {
   let actualSquares = [
     'O', null, null,
     null, 'X', null,
-    null, null, null,
+    null, null, null
   ];
-  let isCpuWinner;
-  let isPlayerWinner;
   test('if cpu can not win the game', () => {
-    isCpuWinner = false;
     const newBoard = autoPlay(actualSquares);
     const expected = [
       'O', 'O', null,
       null, 'X', null,
-      null, null, null,
+      null, null, null
     ];
     expect(newBoard).toEqual(expected);
   });
   test('if cpu can not win the game and player either', () => {
-    isCpuWinner = false;
-    isPlayerWinner = false;
     const newBoard = autoPlay(actualSquares);
     const expected = [
       'O', 'O', null,
       null, 'X', null,
-      null, null, null,
+      null, null, null
     ];
     expect(newBoard).toEqual(expected);
   });
   test('and there are no player on center', () => {
-    isCpuWinner = false;
-    isPlayerWinner = false;
     actualSquares = [
       'O', 'X', null,
       null, null, null,
-      null, null, null,
+      null, null, null
     ];
     const newBoard = autoPlay(actualSquares);
     const expected = [
       'O', 'X', null,
       null, 'O', null,
-      null, null, null,
+      null, null, null
     ];
     expect(newBoard).toEqual(expected);
   });

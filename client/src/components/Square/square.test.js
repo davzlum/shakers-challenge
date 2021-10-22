1cdec92f8fc99d15d90904ab695594744b21c461
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-22 20:03:10 +0200

    Test square component

--- client/src/components/Square/square.test.js
+++ client/src/components/Square/square.test.js
@@ -6,15 +6,15 @@ import { render, screen, fireEvent } from '../../utils/testUtils';
 
 describe('Square component', () => {
   beforeEach(() => {
-    render(<Square value={null} squareClick={jest.fn()} turn="X" />);
+    render(<Square value={null} squareClick={jest.fn()} turn="X" position={1} />);
   });
   test('Must contain an element', async () => {
-    expect(screen.getByTestId('square-box')).toBeInTheDocument();
+    expect(screen.getByTestId('square-1')).toBeInTheDocument();
   });
   test('onClick you assign value to a square', () => {
     const handleClick = jest.fn();
     jest.mock(handleClick());
-    const square = screen.getByTestId('square-box');
+    const square = screen.getByTestId('square-1');
     fireEvent.click(square);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });

df191634129ffef81cf659c19fc7acd2a7887f04
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-10-22 02:59:25 +0200

    Refactor function autoplay

--- server/src/helpers/game.helper.js
+++ server/src/helpers/game.helper.js
@@ -3,7 +3,7 @@ const winningPositions = require('../const/winningPositions');
 const checkForWinner = (newSquares) => {
   let result = {
     isWinner: false,
-    isTie: false,
+    isTie: false
   };
 
   winningPositions.forEach((position) => {
@@ -13,88 +13,56 @@ const checkForWinner = (newSquares) => {
       result = {
         ...result,
         isWinner: true,
-        player: newSquares[top],
+        player: newSquares[top]
       };
     }
   });
   if (!newSquares.includes(null)) {
     result = {
       ...result,
-      isTie: true,
+      isTie: true
     };
   }
 
   return result;
 };
 
-// const simulate = (actualSquares, playerToCheck) => {
-//   const fakeSquares = [...actualSquares];
-//   for (let i = 0; i < actualSquares.length; i += 1) {
-//     if (!actualSquares[i]) {
-//       fakeSquares[i] = playerToCheck;
-//       const result = checkForWinner(fakeSquares);
-//       if (result.isWinner) {
-//         return { isWinner: result.isWinner, position: i };
-//       }
-//       fakeSquares[i] = null;
-//     }
-//   }
-// };
+const simulate = (actualSquares, playerToCheck) => {
+  const fakeSquares = [...actualSquares];
+  for (let i = 0; i < actualSquares.length; i += 1) {
+    if (!actualSquares[i]) {
+      fakeSquares[i] = playerToCheck;
+      const result = checkForWinner(fakeSquares);
+      if (result.isWinner) {
+        return { isWinner: result.isWinner, position: i };
+      }
+      fakeSquares[i] = null;
+    }
+  }
+  return { isWinner: false, position: null };
+};
 
 const autoPlay = (actualSquares) => {
   const newSquares = [...actualSquares];
-
-  let isCpuWinner = false;
-  let isPlayerWinner = false;
-
-  for (let i = 0; i < actualSquares.length; i += 1) {
-    if (!actualSquares[i]) {
-      newSquares[i] = 'O';
-      const result = checkForWinner(newSquares);
-      if (result.isWinner) {
-        isCpuWinner = true;
-        break;
-      } else {
-        newSquares[i] = null;
-      }
-    }
-  }
-  // const cpuSimulation = simulate(actualSquares, 'O');
-  // const playerSimulation = simulate(actualSquares, 'X');
-
-  // if (cpuSimulation.isWinner) {
-  //   newSquares[cpuSimulation.position] = 'O';
-  // }
-
-  if (!isCpuWinner) {
-    // newSquares[playerSimulation.position] = 'O';
-    for (let i = 0; i < actualSquares.length; i += 1) {
-      if (!actualSquares[i]) {
-        newSquares[i] = 'X';
-        const result = checkForWinner(newSquares);
-        if (result.isWinner) {
-          isPlayerWinner = true;
+  const cpuSimulation = simulate(actualSquares, 'O');
+  if (cpuSimulation.isWinner) {
+    newSquares[cpuSimulation.position] = 'O';
+  } else {
+    const playerSimulation = simulate(actualSquares, 'X');
+    if (playerSimulation.isWinner) {
+      newSquares[playerSimulation.position] = 'O';
+    } else {
+      for (let i = 0; i < actualSquares.length; i += 1) {
+        if (!actualSquares[4]) {
+          newSquares[4] = 'O';
+          break;
+        } else if (actualSquares[4] && !actualSquares[i]) {
           newSquares[i] = 'O';
           break;
-        } else {
-          newSquares[i] = null;
         }
       }
     }
   }
-  if (!isCpuWinner && !isPlayerWinner) {
-    // newSquares[cpuSimulation.position] = 'O';
-    for (let i = 0; i < actualSquares.length; i += 1) {
-      if (!actualSquares[4]) {
-        newSquares[4] = 'O';
-        break;
-      } else if (actualSquares[4] && !actualSquares[i]) {
-        newSquares[i] = 'O';
-        break;
-      }
-    }
-  }
-
   return newSquares;
 };
 
